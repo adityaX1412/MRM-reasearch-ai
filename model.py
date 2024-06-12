f6549bacec4568ae3668e4ee2142b5785ffe144e
adityaX1412 <148233635+adityaX1412@users.noreply.github.com>
2024-06-12 23:48:01 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,34 @@
+# model.py
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+class Network(nn.Module):
+    def __init__(self):
+        super(Network, self).__init__()
+        self.convolutaional_neural_network_layers = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=12, kernel_size=3, padding=1, stride=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(in_channels=12, out_channels=24, kernel_size=3, padding=1, stride=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.linear_layers = nn.Sequential(
+            nn.Linear(in_features=24*7*7, out_features=64),
+            nn.ReLU(),
+            nn.Dropout(p=0.2),
+            nn.Linear(in_features=64, out_features=10)
+        )
+
+    def forward(self, x):
+        x = self.convolutaional_neural_network_layers(x)
+        x = x.view(x.size(0), -1)
+        x = self.linear_layers(x)
+        x = F.log_softmax(x, dim=1)
+        return x
+
+if __name__ == "__main__":
+    model = Network()
+    print(model)
